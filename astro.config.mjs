46081fb9e39a01f14338c231daa4d9335b13d9d5
Tanmay Shah <tanmay.shah@oslash.com>
2023-06-17 18:07:59 +0530

    fix: change base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   // used to generate images
   site: 'https://tanmay-d-shah.github.io',
-  base: '/personal-portfolio',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
 });
